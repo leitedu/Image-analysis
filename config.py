9fec2307ba548b6c90bd51f8296eda95c0216c56
Eduardo Leite <133655512+leitedu@users.noreply.github.com>
2026-07-21 18:45:37 -0300

    Refactor culture and DBSCAN parameters into dictionaries

--- config.py
+++ config.py
@@ -1,13 +1,16 @@
 #Cell cultures parameters
-hr = ['24h', '48h'] #cultivation time
-concentracoes = ['04.5', '09.0', '18.0', '25.0', '32.0'] #silica concentration mM
-
+culture_parameters = {
+                        time: ['24h', '48h'], #cultivation time
+                        concentration : ['04.5', '09.0', '18.0', '25.0', '32.0'] #silica concentration [mM]
+                      }
 #Luma
 param_luma = [0.299, 0.587, 0.114]
 
 #DBSCAN
-limiar = 50
-eps = 5
-min_samples = 20
+dbsca_parameters = {
+                        dblimiar: 50, #cultivation time
+                        eps : 5,
+                        min_samples : 20 #silica concentration [mM]
+                      }
 
 folder = r'./images' #folder of image cultures
